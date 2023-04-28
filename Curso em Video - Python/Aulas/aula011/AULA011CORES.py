43f43471318ab082630f4ceda0229b955c76996c
Costajosue <conta2josue@gmail.com>
2023-04-28 20:20:39 -0300

    aula011 sobre cores

---
+++ Curso em Video - Python/Aulas/aula011/AULA011CORES.py
@@ -0,0 +1,26 @@
+    # CORES
+
+print('\033[1;36;41m ola, mundo!')
+
+     # SEMPRE DEVEMOS USAR O COMANDO \033[M 
+     # SE OPITAR POR COLORIR SOMENTE AS PALAVRAS DEVEMOS USAR:
+print('\033[1;36;41m ola, mundo!\033[m]')
+
+#-------------------------------------------------------------------------------------
+
+a = 3
+b = 4
+print('Os valores são\033[32m {}\033[m e \033[31m{}\033[m!'.format(a, b))
+
+#-------------------------------------------------------------------------------------
+
+nome = 'josué'
+print('olá! Muito prazer em te conhecer, {}{}{}!'.format('\033[4;34m', nome, '\033[m'))
+
+#-------------------------------------------------------------------------------------
+nome = 'josué'
+cores = {'limpa':'\033[m',
+         'azul':'\033[34m',
+         'amarelo':'\033[33m',
+         'pretoebranco':'\033[7;30m'}
+print('olá! Muito prazer em te conhecer, {}{}{}!'.format(cores['amarelo'], nome, cores['limpa']))
