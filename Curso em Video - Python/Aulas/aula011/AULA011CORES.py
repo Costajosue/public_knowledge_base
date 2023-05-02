a475ac96b7d77a55f05c20d4c811d275c220d1d7
Costajosue <conta2josue@gmail.com>
2023-05-02 20:19:06 -0300

    Aula012

--- Curso em Video - Python/Aulas/aula011/AULA011CORES.py
+++ Curso em Video - Python/Aulas/aula011/AULA011CORES.py
@@ -1,23 +1,23 @@
-    # CORES
+#     # CORES
 
-print('\033[1;36;41m ola, mundo!')
+# # print('\033[1;36;41m ola, mundo!')
 
-     # SEMPRE DEVEMOS USAR O COMANDO \033[M 
-     # SE OPITAR POR COLORIR SOMENTE AS PALAVRAS DEVEMOS USAR:
-print('\033[1;36;41m ola, mundo!\033[m]')
+# #      # SEMPRE DEVEMOS USAR O COMANDO \033[M 
+# #      # SE OPITAR POR COLORIR SOMENTE AS PALAVRAS DEVEMOS USAR:
+# # print('\033[1;36;41m ola, mundo!\033[m]')
 
-#-------------------------------------------------------------------------------------
+# #-------------------------------------------------------------------------------------
 
-a = 3
-b = 4
-print('Os valores são\033[32m {}\033[m e \033[31m{}\033[m!'.format(a, b))
+# a = 3
+# b = 4
+# print('Os valores são\033[32m {}\033[m e \033[31m{}\033[m!'.format(a, b))
 
-#-------------------------------------------------------------------------------------
+# #-------------------------------------------------------------------------------------
 
-nome = 'josué'
-print('olá! Muito prazer em te conhecer, {}{}{}!'.format('\033[4;34m', nome, '\033[m'))
+# nome = 'josué'
+# print('olá! Muito prazer em te conhecer, {}{}{}!'.format('\033[4;34m', nome, '\033[m'))
 
-#-------------------------------------------------------------------------------------
+ #-------------------------------------------------------------------------------------
 nome = 'josué'
 cores = {'limpa':'\033[m',
          'azul':'\033[34m',
---
+++ Curso em Video - Python/Aulas/aula012.py
@@ -0,0 +1,14 @@
+# AULA SOBRE CONDIÇÕES ANINHADAS.
+
+nome = str(input('Qual é seu nome?'))
+if nome == 'Gustavo':
+    print('Seu nome é incrivel!')
+elif nome == 'pedro' or nome == 'maria':
+    print('Seu nome é muito normal aqui no Brasil') 
+elif nome == 'josue':
+    print('Ola, meu amigo')
+else:
+    print('Seu nome é norma!')    
+print('Tenha um otimo dia {}'.format(nome))
+
+
