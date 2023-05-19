7d1e64d955d5ef67a59e66fbc98c5eb8e0d195da
Costajosue <conta2josue@gmail.com>
2023-05-18 21:18:28 -0300

    aula013

---
+++ Python/Curso em video - Mundo II/Aulas/aula013.py
@@ -0,0 +1,19 @@
+'''
+for c in range(0,6):
+    print(' Olá Mundo')
+print('Fim')                 
+
+''''''_____________________________________________________________________________________________________'''
+'''n = int(input('Digite um número :'))
+for c in range(0, n+1):
+    print(c)
+print('Fim')  '''
+
+#___________________________________________________________________________________________________________
+
+i = int(input('inicio:'))
+f = int(input('Fim:'))
+p = int(input('passo:'))
+for c in range(i, f+1, p):
+    print(c)
+print('Fim')   
