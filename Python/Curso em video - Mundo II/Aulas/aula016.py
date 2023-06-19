5b89499737912ff2c317a66d401e0b94fe32eab2
Costajosue <conta2josue@gmail.com>
2023-06-19 20:26:14 -0300

    Aula16

---
+++ Python/Curso em video - Mundo II/Aulas/aula016.py
@@ -0,0 +1,26 @@
+# lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+# Tuplas são imutaveis -> não podem ser mudadas quando o programa esta rodando...
+# print(lanche)
+
+#  lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#  for cont in range(0, len(lanche)):
+#      print(f'Eu vou comer {lanche [cont]}')
+#  print('Nossa comi bastente !')   
+
+#  lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#  for comida in lanche:
+#      print(f' Eu vou comer {comida}')
+#  print('Ufaaa comi bastante!')   
+
+#  lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#  for pos, comida in enumerate(lanche):
+#      print(f' Eu vou comer {comida} na posição {pos}')
+#  print('Ufaaa comi bastante!')   
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#print(sorted(lanche))
+# o comanda sorted pode colocar em ordem e organiza a variavel em ordem alfabetica 
+
+#pessoa = ('Gustavo', 'josue', '99')
+#del(pessoa) => vc não pode alterar uma tupla mas pode apagar ela.. com o comando del
+#print(pessoa)
