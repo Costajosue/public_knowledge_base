50943b0edd1877ee435d920f6cf2307d3cff62b4
Costajosue <conta2josue@gmail.com>
2023-08-25 19:40:36 -0300

    Arrumando o repositorio

--- Bc Systems Analysis & Development/.Python/Aa04(part 03).py
+++ Bc Systems Analysis & Development/.Python/Aa04(part 03).py
@@ -1,6 +1,8 @@
 import pandas as pd  
 import matplotlib.pyplot as plt #importando a biblioteca 
-dados = {
+
+# Dados passado pela atividade 
+dados = { 
 'X': [1,2,3,4,5,6],
 'Y1': [120,110,130,145,118,125],
 'Y2': [95,54,86,77,90,81]
@@ -8,12 +10,13 @@ dados = {
 
 
 
-df = pd.DataFrame(data=dados)# transformando em uma DataFrame
+df = pd.DataFrame(data=dados) # transformando em uma DataFrame
 # print(df)
-# Criando um gráfico
 
+# Criando um gráfico
 plt.plot(df) 
-# Atribuindo um título ao gráfico
+
+# Atribuindo um título ao gráfico 
 plt.title('Exemplo utilizando Atividade 04')
 plt.xlabel('Variavel 1')
 plt.ylabel('Variavel 2')
--- Bc Systems Analysis & Development/Linguagem_C/Ta3.c
+++ Bc Systems Analysis & Development/Linguagem_C/Ta3.c
@@ -1,3 +1,16 @@
-/*      EXEMPLO 01    */
 #include <stdio.h>
+#include <stdlib.h>
 
+int main () {
+    
+    int *p;
+    p=(int *) malloc(sizeof(int));
+    
+    if (!p) {
+        printf("Erro de memoria insuficiente");
+    }else{
+        printf("Memoria alocada com sucesso");
+    }
+    
+    return 0;
+}
