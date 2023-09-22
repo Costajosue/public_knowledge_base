d9f07c7828916467842ad015e3d44fc1c652008a
Costajosue <conta2josue@gmail.com>
2023-09-21 21:09:03 -0300

    Aula 20 Função

--- Python/Curso em video - Mundo III/Aulas/aula020.py
+++ Python/Curso em video - Mundo III/Aulas/aula020.py
@@ -4,5 +4,46 @@
 
 
 # ex:
-def mostrarLinha():
-    print('---------------------------')
+#def mostrarLinha():
+#    print('---------------------------')
+    
+# def soma(a, b):
+#     print(f'A = {a} e B = {b}')
+#     s = a + b
+#     print(f'A soma A + B = {s}')
+
+
+# # Programa principal:
+# soma(b=4, a=5)
+# soma(7, 2)
+
+# def contador(*num):
+#     for valor in num:
+#         print(f'{valor} ',end='')
+#     print('Fim!')
+
+
+# contador(2, 1, 7)
+# contador(8, 2)
+# contador(4, 4, 7, 6, 2)
+
+
+# def contador(*num):
+#      tam = len(num)
+#      print(f'Recebi os valores {num} e são todos {tam} números')
+
+
+# contador(2, 1, 7)
+# contador(8, 2)
+# contador(4, 4, 7, 6, 2)
+
+
+# def soma(* valores):
+#     s = 0
+#     for num in valores:
+#         s += num
+#     print(f'somando os valores {valores} temos {s}')
+
+
+# soma(5, 2)
+# soma(2, 9, 4)
