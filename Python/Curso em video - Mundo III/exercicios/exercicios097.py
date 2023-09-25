c80f183359bb114dcbce81a2260ad72fe098c751
Costajosue <conta2josue@gmail.com>
2023-09-25 20:44:29 -0300

    Exercicio 097 - função

---
+++ Python/Curso em video - Mundo III/exercicios/exercicios097.py
@@ -0,0 +1,11 @@
+# criando uma variaVel
+def escreva (MENSAGEM):
+    tamanho = len (MENSAGEM) + 4
+    print('~' * tamanho)
+    print(f'{MENSAGEM}')
+    print('~' * tamanho)
+
+
+# Programa principal 
+escreva('Josué Costa SIlva')
+escreva('Estudo de ADS')
