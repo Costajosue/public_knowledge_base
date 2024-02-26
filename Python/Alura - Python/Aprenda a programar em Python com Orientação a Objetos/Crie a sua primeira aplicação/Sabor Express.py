bdf5f96f3f80e735b36202ff51d6257a6d0d2873
Costajosue <conta2josue@gmail.com>
2024-02-26 20:36:36 -0300

    Atualizando Projeto 'Sabor Express'

--- Python/Alura - Python/Aprenda a programar em Python com Orientação a Objetos/Crie a sua primeira aplicação/Sabor Express.py
+++ Python/Alura - Python/Aprenda a programar em Python com Orientação a Objetos/Crie a sua primeira aplicação/Sabor Express.py
@@ -10,11 +10,19 @@ print("""
 
 # Criando menu do projeto:
 print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair')
+print('2. Listar restaurantes')
+print('3. Ativar restaurantes')
+print('4. Sair\n')
 
 # Deixando o menu funcional armarzenando o input em uma variavel e logo após mestramos o resultado com o print:
 opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção: {opcao_escolhida}')
- 
+
+# Agora utilizando o if, elif e else para colocarmoss funcionalidades nas opções.
+if opcao_escolhida == 1:
+    print('Cadastrar restaurantes')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurantes')
+else:
+    print('Encerrando o programa')
