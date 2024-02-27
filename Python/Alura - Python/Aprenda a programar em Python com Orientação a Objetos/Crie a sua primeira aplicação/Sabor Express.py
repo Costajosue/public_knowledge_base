49fe8f969fe4d668d2ba1e48a7bf9b00ea4abe1b
Costajosue <conta2josue@gmail.com>
2024-02-26 21:25:12 -0300

    Atualizando projeto Sabor express

--- Python/Alura - Python/Aprenda a programar em Python com Orientação a Objetos/Crie a sua primeira aplicação/Sabor Express.py
+++ Python/Alura - Python/Aprenda a programar em Python com Orientação a Objetos/Crie a sua primeira aplicação/Sabor Express.py
@@ -1,3 +1,6 @@
+# Importando biblioteca 'OS' que nesse caso vamos utilizar para curtomizar nosso def finalizar_app(): para limpar o terminal quando optarmos pela opção de finaliza o programa\sair mostrar somente 'Finalizando programa' .
+import os
+
 print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -14,10 +17,22 @@ print('2. Listar restaurantes')
 print('3. Ativar restaurantes')
 print('4. Sair\n')
 
-# Deixando o menu funcional armarzenando o input em uma variavel e logo após mestramos o resultado com o print:
-opcao_escolhida = input('Escolha uma opção: ')
 
-# Agora utilizando o if, elif e else para colocarmoss funcionalidades nas opções.
+
+# Deixando o menu funcional armarzenando o input em uma variavel e logo após mestramos o resultado com o print:
+# Adicionando o int na frete de input significa que estamos mostrando que vamos receber um número inteiro.
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+
+
+# Definindo uma função finalizar_app() para finalizar o programa na linha 34, e assim deixar o codigo mais limpo.
+def finalizar_app():
+    os.system('cls') # Utilizando biblioteca os para limpar terminal e mostrar somente 'Finalizando programa'
+    print('Finalizando o App\n')
+
+
+
+# Agora utilizando o if, elif e else para colocarmos funcionalidades nas opções.
 if opcao_escolhida == 1:
     print('Cadastrar restaurantes')
 elif opcao_escolhida == 2:
@@ -25,4 +40,4 @@ elif opcao_escolhida == 2:
 elif opcao_escolhida == 3:
     print('Ativar restaurantes')
 else:
-    print('Encerrando o programa')
+    finalizar_app()
