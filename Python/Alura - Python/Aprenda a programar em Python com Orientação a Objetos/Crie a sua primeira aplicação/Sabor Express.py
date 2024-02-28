db4737d1bf75ca882037791121a47afd5f424968
Costajosue <conta2josue@gmail.com>
2024-02-27 21:15:11 -0300

    melhorando projeto sabor express

--- Python/Alura - Python/Aprenda a programar em Python com Orientação a Objetos/Crie a sua primeira aplicação/Sabor Express.py
+++ Python/Alura - Python/Aprenda a programar em Python com Orientação a Objetos/Crie a sua primeira aplicação/Sabor Express.py
@@ -1,28 +1,22 @@
 # Importando biblioteca 'OS' que nesse caso vamos utilizar para curtomizar nosso def finalizar_app(): para limpar o terminal quando optarmos pela opção de finaliza o programa\sair mostrar somente 'Finalizando programa' .
 import os
 
-print("""
-░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
-██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
-╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
-░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
-██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
-""")
+def exibir_nome_do_programa():
+    print("""
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+    """)
 
-
-# Criando menu do projeto:
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurantes')
-print('4. Sair\n')
-
-
-
-# Deixando o menu funcional armarzenando o input em uma variavel e logo após mestramos o resultado com o print:
-# Adicionando o int na frete de input significa que estamos mostrando que vamos receber um número inteiro.
-opcao_escolhida = int(input('Escolha uma opção: '))
-
+def exibir_opçoes(): # Criando menu do projeto:
+    # Criando menu do projeto:
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurantes')
+    print('4. Sair\n')
 
 
 # Definindo uma função finalizar_app() para finalizar o programa na linha 34, e assim deixar o codigo mais limpo.
@@ -30,14 +24,25 @@ def finalizar_app():
     os.system('cls') # Utilizando biblioteca os para limpar terminal e mostrar somente 'Finalizando programa'
     print('Finalizando o App\n')
 
+def escolher_opcao():
+    # Deixando o menu funcional armarzenando o input em uma variavel e logo após mestramos o resultado com o print:
+    # Adicionando o int na frete de input significa que estamos mostrando que vamos receber um número inteiro.
+    opcao_escolhida = int(input('Escolha uma opção: '))
 
+    # Agora utilizando o if, elif e else para colocarmos funcionalidades nas opções.
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurantes')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurantes')
+    else:
+        finalizar_app()
 
-# Agora utilizando o if, elif e else para colocarmos funcionalidades nas opções.
-if opcao_escolhida == 1:
-    print('Cadastrar restaurantes')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurantes')
-else:
-    finalizar_app()
+def main():
+    exibir_nome_do_programa()
+    exibir_opçoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
