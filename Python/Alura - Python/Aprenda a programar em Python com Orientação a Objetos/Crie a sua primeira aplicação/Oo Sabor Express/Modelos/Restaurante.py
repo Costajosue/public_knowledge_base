42b2324faf64c332308063ac38d88a75a2268ae6
Costajosue <conta2josue@gmail.com>
2024-03-14 21:07:31 -0300

    Estudando programação O.o

--- Python/Alura - Python/Aprenda a programar em Python com Orientação a Objetos/Crie a sua primeira aplicação/Oo Sabor Express/Modelos/Restaurante.py
+++ Python/Alura - Python/Aprenda a programar em Python com Orientação a Objetos/Crie a sua primeira aplicação/Oo Sabor Express/Modelos/Restaurante.py
@@ -1,16 +1,23 @@
 
-class Restaurante: # Criando uma classe no python , ' Objeto é a instancia de uma classe '
-    nome = ''
-    categoria = ''
-    ativo = False
-
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+class Restaurante:# Criando uma classe no python , ' Objeto é a instancia de uma classe '
+    restaurantes = []
 
 
-restaurante_pizza = Restaurante()
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+    
 
-Restaurantes = [ restaurante_pizza, restaurante_praca ]
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restalrantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
-print(vars(restaurante_praca))
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+Restaurante.listar_restalrantes() 
