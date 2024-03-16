a2174723b472e6de1148397e30c9eae0be2ca482
Costajosue <conta2josue@gmail.com>
2024-03-15 21:32:35 -0300

    projeto de java faculdade

---
+++ Bc Systems Analysis & Development/.Java/Projeto aula 01/Administrativo.java
@@ -0,0 +1,5 @@
+public class Administrativo extends Pessoa {
+
+ public String funcao;
+
+}
---
+++ Bc Systems Analysis & Development/.Java/Projeto aula 01/Pessoa.java
@@ -0,0 +1,19 @@
+public class Pessoa {
+
+    public String nome;
+    public String sexo;
+    public Long CPF;
+   
+   
+    //Método 1
+    public void comer()
+    {
+    System.out.println("Estou com fome!");
+    }
+   
+    //Método 2
+    public void trabalhar()
+    {
+    System.out.println("O/a colaborador(a) " + nome + " vai trabalhar");
+    }
+   }
