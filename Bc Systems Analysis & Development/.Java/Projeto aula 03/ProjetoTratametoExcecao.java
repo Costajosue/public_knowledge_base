6e5fc95b8633c67a658636541dfa10d91be561db
Costajosue <conta2josue@gmail.com>
2024-03-19 21:01:49 -0300

    Praticando Java

---
+++ Bc Systems Analysis & Development/.Java/Projeto aula 03/ProjetoTratametoExcecao.java
@@ -0,0 +1,18 @@
+public class ProjetoTratametoExcecao {
+
+    public static void main(String[] args) {
+        
+        /* 
+        diminuiLetras ltpqn = new diminuiLetras(); //Chamando classe e criando um objeto//
+        ltpqn.lowCase();
+        */
+
+        try {
+            diminuiLetras ltpqn = new diminuiLetras();
+            ltpqn.lowCase();
+        } catch (NullPointerException e) {
+            System.out.println("Exceção NullPointerException indentificada");
+            System.out.println("Metodo diminuiLetras() tratado\n" +e);
+        }
+    }
+}
---
+++ Bc Systems Analysis & Development/.Java/Projeto aula 03/diminuiLetras.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class diminuiLetras {
+    
+    public static void lowCase() throws NullPointerException{
+           
+        String word = null;
+        String newWord = null;
+
+        word = JOptionPane.showInputDialog(null, "Digite um nome: ");
+
+        newWord = word.toLowerCase(); //Crianda metodo//
+        System.out.println("A palavra informada é: " +word);
+        System.out.println("A palavra corrigida é: " +newWord);
+    }
+}
