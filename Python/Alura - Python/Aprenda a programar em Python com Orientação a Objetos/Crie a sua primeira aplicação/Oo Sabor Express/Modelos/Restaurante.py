e3fd35bcf073b7d226be067e561ffac64392c76b
Costajosue <conta2josue@gmail.com>
2024-03-21 21:26:45 -0300

    atualizando projeto alura restaurante

--- Python/Alura - Python/Aprenda a programar em Python com Orientação a Objetos/Crie a sua primeira aplicação/Oo Sabor Express/Modelos/Restaurante.py
+++ Python/Alura - Python/Aprenda a programar em Python com Orientação a Objetos/Crie a sua primeira aplicação/Oo Sabor Express/Modelos/Restaurante.py
@@ -6,7 +6,7 @@ class Restaurante:# Criando uma classe no python , ' Objeto é a instancia de um
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self)
     
 
@@ -15,9 +15,13 @@ class Restaurante:# Criando uma classe no python , ' Objeto é a instancia de um
     
     def listar_restalrantes():
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-Restaurante.listar_restalrantes() 
+Restaurante.listar_restalrantes()
