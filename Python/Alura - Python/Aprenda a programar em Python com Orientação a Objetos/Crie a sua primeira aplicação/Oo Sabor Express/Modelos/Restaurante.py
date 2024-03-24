535843ba42718cdfca293b8c82593f0843bc7993
Costajosue <conta2josue@gmail.com>
2024-03-24 12:21:41 -0300

    Aprofundando em propriedades em python

--- Python/Alura - Python/Aprenda a programar em Python com Orientação a Objetos/Crie a sua primeira aplicação/Oo Sabor Express/Modelos/Restaurante.py
+++ Python/Alura - Python/Aprenda a programar em Python com Orientação a Objetos/Crie a sua primeira aplicação/Oo Sabor Express/Modelos/Restaurante.py
@@ -4,24 +4,25 @@ class Restaurante:# Criando uma classe no python , ' Objeto é a instancia de um
 
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
     
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self.categoria}'
     
     def listar_restalrantes():
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '⌧' if self._ativo else '☐'
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+restaurante_praca.nome = 'Praça 2.0'
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
 
 Restaurante.listar_restalrantes()
