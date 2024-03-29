668effd4e89d49c06e47cc7e62271d47a5120cce
Costajosue <conta2josue@gmail.com>
2024-03-29 12:42:19 -0300

    Organizado pastas

--- Python/Alura - Python/Aprenda a programar em Python com O.o/Crie a sua primeira aplicação/Modelos/Modelos/Restaurante.py
+++ Python/Alura - Python/Aprenda a programar em Python com O.o/Crie a sua primeira aplicação/Modelos/Modelos/Restaurante.py
@@ -13,16 +13,15 @@ class Restaurante:# Criando uma classe no python , ' Objeto é a instancia de um
     def __str__(self):
         return f'{self._nome} | {self.categoria}'
     
-    def listar_restalrantes():
+    @classmethod
+    def listar_restalrantes(cls):
         for restaurante in Restaurante.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '⌧' if self._ativo else '☐'
-
-restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.nome = 'Praça 2.0'
-restaurante_pizza = Restaurante('pizza express', 'Italiana')
-
-Restaurante.listar_restalrantes()
+    
+    # Criando metodo para alternar estado de "ATIVO\DESATIVADO"
+    def alternar_estado(self):
+        self._ativo = not self._ativo
