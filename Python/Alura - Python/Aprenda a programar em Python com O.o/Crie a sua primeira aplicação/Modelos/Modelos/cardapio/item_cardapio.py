fcb9f0e7550b560ad4ccf1071517842a91f4fa09
Costajosue <conta2josue@gmail.com>
2024-05-01 21:08:52 -0300

    Metodo Abstrados

--- Python/Alura - Python/Aprenda a programar em Python com O.o/Crie a sua primeira aplicação/Modelos/Modelos/cardapio/item_cardapio.py
+++ Python/Alura - Python/Aprenda a programar em Python com O.o/Crie a sua primeira aplicação/Modelos/Modelos/cardapio/item_cardapio.py
@@ -1,6 +1,12 @@
+# Importando uma biblioteca para metodos abstratos
+from abc import ABC, abstractmethod
 
 # Criando uma classe para os itens do cardapio. Juntamente com um construtor __Init__ com as informações base que teremos na classe.
 class ItemCardapio:
     def __init__(self,nome,preco):
         self._nome = nome # -> colocando o _ para indicar que esses valores e esclusivo do item_cardapio.
         self._preco = preco
+
+    @abstractmethod
+    def aplicar_desconto(self):
+        pass
