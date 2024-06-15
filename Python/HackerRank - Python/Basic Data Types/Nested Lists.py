4a358984d70ddbaa324ec935ce64d729dbb1df0c
Costajosue <conta2josue@gmail.com>
2024-06-15 14:40:43 -0300

    exercicio nested lists finalizado

--- Python/HackerRank - Python/Basic Data Types/Nested Lists.py
+++ Python/HackerRank - Python/Basic Data Types/Nested Lists.py
@@ -1,24 +1,11 @@
 if __name__ == '__main__':
         estudantes = []
 
-        for _ in range(int(input(f'Digite a quantidade de alunos: '))):
-                nome = str(input(f'Digite o nome do aluno: '))
-                nota = float(input(f'Por favor digite a nota do aluno: '))
+        for _ in range(int(input())):
+                nome = str(input())
+                nota = float(input())
                 estudantes.append([nome, nota])
         notas = sorted(set(nota for nome, nota in estudantes)) 
-        # print(sorted(nome for nome, nota in estudantes if nota == notas[1]))
         menores = sorted(nome for nome, nota in estudantes if nota == notas[1])
         for i in menores:
-                print(f'{i}\n')
-
-# 5
-# Harry
-# 37.21
-# Berry
-# 37.21
-# Tina
-# 37.2
-# Akriti
-# 41
-# Harsh
-# 39
+                print(i)
