36cd3a7b15bfccbb38eff3a3cee9ea73389af8b9
Costajosue <conta2josue@gmail.com>
2024-06-19 17:31:36 -0300

    exercicio Find the Runner-Up Score! finalizado

--- Python/HackerRank - Python/Basic Data Types/Find the Runner-Up Score.py
+++ Python/HackerRank - Python/Basic Data Types/Find the Runner-Up Score.py
@@ -1,3 +1,13 @@
+# Encontrar o segundo colocado #
+
+
 if __name__ == '__main__':
+    notas = []
+    # imputar o valor de 'n':
     n = int(input())
-    arr = map(int, input().split())
+    # definindo a Ponduação dos alunos e utilizando o map no lugar de uma for e a função split para colocarmos um espaço entre cada nota.
+    pontuação = map(int, input().split())
+    # Criando uma lista e adc as pontuações ja colocando em ordem descresente com o sort e reverse=True
+    notas = list(set(pontuação)) # Utilizando a função set para excluir possiveis notas duplicadas.
+    notas.sort(reverse=True)# sort para organizar em ordem e 'reverse=True' para ficar de dorma descresente.
+print(notas[1])
