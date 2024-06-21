fe730c37d7d33f3e5580f141d929e1bfa8098683
Costajosue <conta2josue@gmail.com>
2024-06-21 14:29:49 -0300

    solução dois do exercicio de tuples

--- Python/HackerRank - Python/Basic Data Types/Tuples.py
+++ Python/HackerRank - Python/Basic Data Types/Tuples.py
@@ -1,11 +1,19 @@
 
-n = int(input())  # Ler o número de elementos na tupla
+# n = int(input())  # Ler o número de elementos na tupla
 
-# Ler os inteiros separados por espaços utilizando o split() para separar estou utilizando a função map() para que 
-# Dentro da minha tupla todos os input() sejar transformados em um numero inteiro.
-t = tuple(map(int, input().split())) 
+# # Ler os inteiros separados por espaços utilizando o split() para separar estou utilizando a função map() para que 
+# # Dentro da minha tupla todos os input() sejar transformados em um numero inteiro.
+# t = tuple(map(int, input().split())) 
 
-# Calculando a hash
-hash_n = hash(t)
+# # Calculando a hash
+# hash_n = hash(t)
 
-print(hash_n)
+# print(hash_n)
+
+
+n = int(input())
+t = (map(int, input().split())) 
+
+t2 = tuple(t)
+
+print(hash(t2))
