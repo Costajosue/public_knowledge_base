d65402e581f12ef03bbd5c2e05248c845eea4eeb
Costajosue <conta2josue@gmail.com>
2024-06-27 17:41:09 -0300

    finalizando exercicio sobre strings

---
+++ Python/HackerRank - Python/Strings/What's Your Name.py
@@ -0,0 +1,19 @@
+
+
+nome = str(input())
+sobrenome = str(input())
+print(f'Hello {nome} {sobrenome}! You just delved into python.')
+        
+# Ross
+# Taylor
+
+# OU UTILIZANDO O MODELO DO HACKERRANK
+
+def print_full_name(first, last):
+    # Write your code here
+    print(f"Hello {first} {last}! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
