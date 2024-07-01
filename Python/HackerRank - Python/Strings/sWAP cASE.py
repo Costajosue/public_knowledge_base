f6556e1624a6e62097add3c2f36b12206c44886f
Costajosue <conta2josue@gmail.com>
2024-07-01 20:49:57 -0300

    Finalizando o exercicio sobre swapcase

---
+++ Python/HackerRank - Python/Strings/sWAP cASE.py
@@ -0,0 +1,13 @@
+def swap_case(s): # criando uma função def
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input() # input para digitar a frase.
+    result = swap_case(s)
+    print(result)
+
+
+# EXPLICAÇÃO DA FUNÇÃO: swap_case()
+# Função é usada para converter todos os caracteres maiúsculos de uma string para minúsculos 
+# e todos os caracteres minúsculos para maiúsculos.Ela é especialmente útil quando se deseja 
+# alternar o caso de cada caractere em uma string.
