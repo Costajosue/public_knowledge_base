097e66190b6be9173146ff2a8aa47ec9067e4d77
Costajosue <conta2josue@gmail.com>
2024-07-16 20:48:24 -0300

    String Split and Join

---
+++ Python/HackerRank - Python/Strings/String Split and Join.py
@@ -0,0 +1,10 @@
+def split_and_join(line):
+    r = 0
+    r = '-'.join(result)
+    print(r)
+    
+
+    if __name__ == '__main__':
+        line = input()
+        result = split_and_join(line)
+        print(result)
